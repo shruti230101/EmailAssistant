eeea381252cc338d73bc5aa70a1ed5b3628e350f
Shruti Agarwal <shruti_agrwal1@epam.com>
2025-03-03 14:47:04 +0530

    connected frontend for the email assistant

--- emailwriter/src/main/java/com/project/emailwriter/controllers/EmailGeneratorController.java
+++ emailwriter/src/main/java/com/project/emailwriter/controllers/EmailGeneratorController.java
@@ -5,14 +5,12 @@ import com.project.emailwriter.entities.EmailRequest;
 import com.project.emailwriter.services.EmailGeneratorService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins = "*")
 public class EmailGeneratorController {
 
     private final EmailGeneratorService emailGeneratorService;
